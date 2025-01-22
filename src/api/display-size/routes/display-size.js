a256bf188cc5d834c8dc01f1da065c652cad6c20
burakuytun-rightsoft <burakuytun@rightsoft.co.uk>
2025-01-22 01:51:43 +0000

    develop mode

---
+++ src/api/display-size/routes/display-size.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * display-size router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::display-size.display-size');
---
+++ src/api/display-size/services/display-size.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * display-size service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::display-size.display-size');
