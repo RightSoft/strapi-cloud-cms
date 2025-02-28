9ac6cc5f64fe6a0a0be3f64008b9aed2e503b742
burakuytun-rightsoft <burakuytun@rightsoft.co.uk>
2025-02-28 16:09:40 +0100

    file size limit fixed

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,17 @@ module.exports = [
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
-  'strapi::body',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "750mb", // Increase form size
+      jsonLimit: "750mb", // Increase JSON payload size
+      textLimit: "750mb", // Increase text size
+      formidable: {
+        maxFileSize: 750 * 1024 * 1024, // Set max upload size to 500MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
